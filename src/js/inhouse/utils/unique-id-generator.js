2bb6c219326dcc468648876ed946e8b39b7a0201
Mary Solis <mary.solis@financialogixgroup.com>
2016-04-18 00:20:24 -0400

    Adding a safety fallback in case Math.random() ends up generating a big enough number

--- src/js/inhouse/utils/unique-id-generator.js
+++ src/js/inhouse/utils/unique-id-generator.js
@@ -30,7 +30,7 @@ function UniqueIdGenerator()
     this.getUuid = function()
     {
         return 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) {
-            var num = Math.random() *16 | 0, v = c === 'x' ? num : (num&0x3|0x8);
+            var num = Math.min(15, Math.random() *16 | 0), v = c === 'x' ? num : (num&0x3|0x8);
 
             return v.toString(16);
         });
